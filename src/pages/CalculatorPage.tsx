6f09a26073fdc87fc9cf45b467d41124185ce8f8
dmtr636 <dmtr636@gmail.com>
2022-11-24 21:36:03 +0300

    Test

--- src/pages/CalculatorPage.tsx
+++ src/pages/CalculatorPage.tsx
@@ -3,7 +3,7 @@ import React from "react"
 export const CalculatorPage = () => {
     return (
         <div>
-            It's calculator page
+            It's calculator page ...test
         </div>
     )
 }
