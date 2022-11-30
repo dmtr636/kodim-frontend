480df10e3e3dc6469aca65b2e67ad71eb9546ab1
Dmitry Drozdov <drozdov@algont.ru>
2022-11-30 13:37:32 +0300

    Fix

--- src/components/common/ScrollToTop/ScrollToTop.tsx
+++ src/components/common/ScrollToTop/ScrollToTop.tsx
@@ -5,7 +5,9 @@ export default function ScrollToTop() {
     const { pathname, search } = useLocation();
 
     useEffect(() => {
-        window.scrollTo(0, 0);
+        if (pathname === "/calculator") {
+            window.scrollTo(0, 0);
+        }
     }, [pathname, search]);
 
     return null;
