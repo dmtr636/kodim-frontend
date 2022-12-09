0c4fca76bd735019a843da91710a4c0df035c509
KoMRaD1240 <komrad.noob@yandex.ru>
2022-12-09 23:27:31 +0300

    mob ver

--- src/components/main/StartProject/StartProject.jsx
+++ src/components/main/StartProject/StartProject.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Button from "../../common/Button/Button";
 import style from "./StartProject.module.scss";
-import houseBG from "./houseBG.svg"
+import houseBG from "./houseBG.svg";
+import houseBGMob from "./houseBGMob.svg";
 import { useNavigate } from "react-router-dom";
+import useWindowDimensions from "../../../hooks/useWindowDimensions";
 export const StartProject = () => {
-    const navigate=useNavigate()
+  const { width } = useWindowDimensions()
+  const navigate = useNavigate();
   return (
     <div className={style.wrapper}>
       <div className={style.left}>
@@ -13,10 +16,14 @@ export const StartProject = () => {
         </div>
 
         <div className={style.button}>
-          <Button onClick={() => navigate("/calculator")}>Начать свой проект</Button>
+          <Button onClick={() => navigate("/calculator")}>
+            Начать свой проект
+          </Button>
         </div>
       </div>
-      <div className={style.right}><img src={houseBG} alt="" /></div>
+      <div className={style.right}>
+        <img src={width < 700 ?houseBGMob:houseBG} alt="" />
+      </div>
     </div>
   );
 };
