3604e5d86b853637a58eb9ee49de84fd85dfe31f
KoMRaD1240 <komrad.noob@yandex.ru>
2022-12-12 14:09:09 +0300

    changed gif to mp4

--- src/components/main/Services/Desk/Desk.jsx
+++ src/components/main/Services/Desk/Desk.jsx
@@ -2,21 +2,33 @@ import React, { useEffect } from "react";
 import Button from "../../../common/Button/Button";
 import style from "./Desk.module.scss";
 import ServiceButton from "./ServiceButton/ServiceButton";
+import ReactPlayer from "react-player";
+
 import { useNavigate } from "react-router-dom";
-import HTMLcoding from "../Desk/img/HTMLcoding.gif";
-import UXUI from "../Desk/img/UXUI.gif";
-import LOGO from "../Desk/img/LOGO.gif";
-import SEO from "../Desk/img/SEO.gif";
-import TGBOT from "../Desk/img/TGBOT.gif";
-import COPY from "../Desk/img/COPY.gif";
-import CODING from "../Desk/img/CODING.gif";
-import MARKETING from "../Desk/img/MARKETING.gif";
 import useWindowDimensions from "../../../../hooks/useWindowDimensions";
 
+import HTMLcoding from "../Desk/img/verstka.mp4";
+import UXUI from "../Desk/img/uxui.mp4";
+import LOGO from "../Desk/img/logo.mp4";
+import SEO from "../Desk/img/seo.mp4";
+import TGBOT from "../Desk/img/telegram.mp4";
+import COPY from "../Desk/img/copyr.mp4";
+import CODING from "../Desk/img/programmer.mp4";
+import MARKETING from "../Desk/img/MARKETING.mp4";
+
+import HTMLcodingPoster from "../Desk/img/posters/verstka.png";
+import UXUIPoster from "../Desk/img/posters/uxui.png";
+import LOGOPoster from "../Desk/img/posters/logo.png";
+import SEOPoster from "../Desk/img/posters/SEO.png";
+import TGBOTPoster from "../Desk/img/posters/telegram.png";
+import COPYPoster from "../Desk/img/posters/copyr.png";
+import CODINGPoster from "../Desk/img/posters/programmer.png";
+import MARKETINGPoster from "../Desk/img/posters/MARKETING.png";
+
 export const Desk = () => {
-  const {width} = useWindowDimensions()
-  console.log(width)
-  let serviceNameMob=  width < 700 ? "UX‑XUI" : "UX/UI-дизайн"
+  const { width } = useWindowDimensions();
+  console.log(width);
+  let serviceNameMob = width < 700 ? "UX‑XUI" : "UX/UI-дизайн";
   const [activeService, SetActiveService] = React.useState(0);
   const navigate = useNavigate();
   const services = [
@@ -25,48 +37,56 @@ export const Desk = () => {
       serviceText:
         "Визуальная разработка будущего сайта. Составление макета с\xA0учётом особенностей аудитории проекта. Продуманный дизайн позволит вашим клиентам удобно и\xA0комфортно пользоваться продуктом, что благоприятно повлияет на конверсию и общее качество сайта.",
       imgUrl: UXUI,
+      posterUrl: UXUIPoster,
     },
     {
       serviceName: "Копирайтинг",
       serviceText:
         "Написание грамотных коммерческих текстов с целью создания привлекательного информационного содержания на сайте. Продуманный текст поможет привлечь внимание покупателя, ответить на первичные вопросы и сподвигнуть совершить целевое действие",
       imgUrl: COPY,
+      posterUrl: COPYPoster,
     },
     {
       serviceName: "SEO",
       serviceText:
         "СЕО-оптимизация позволяет сайтам быть  понятными для\xA0поисковых систем. Качественное СЕО делает ваш сайт выше и\xA0предпочтительнее в поисковой выдачи. В\xA0таком случае при запросе, пользователь с\xA0большей долей вероятности попадёт в\xA0первую очередь именно к вам.",
       imgUrl: SEO,
+      posterUrl: SEOPoster,
     },
     {
       serviceName: "Логотип",
       serviceText:
         "Для того, чтобы ваш проект был запоминающимся, ему необходим логотип. Он помогает пользователю быстрее ориентироваться и\xA0запоминать ваш сайт. Также логотип даёт общее представление бренда, компании и проекта в целом. Пользователю будет гораздо легче узнать вас в будущем.",
       imgUrl: LOGO,
+      posterUrl: LOGOPoster,
     },
     {
       serviceName: "Вёрстка",
       serviceText:
         "После создания визуального макета проект переходит в стадию разработки. Команда разработчиков на основании макета выполняет вёрстку сайта, используя специальные языки, такие как HTML и CSS. В\xA0результате получается быстрый и\xA0адаптивный сайт, который может быть открыт на любом устройстве.",
       imgUrl: HTMLcoding,
+      posterUrl: CODINGPoster,
     },
     {
       serviceName: "Программирование",
       serviceText:
         "При реализации сложного функционала не обойтись без использования специальных инструментов. Наши разработчики используют для этого самые современные технологии. С\xA0помощью языков программирования прописывается логика работы сайта. Все элементы становятся интерактивными.",
       imgUrl: CODING,
+      posterUrl: COPYPoster,
     },
     {
       serviceName: "Телеграм‑бот",
       serviceText:
         "Все уведомления, связанные с сайтом: заявки, данные для обратной связи или покупка товара, будут приходить на ваш телеграм круглосуточно, с\xA0помощью бота . Вы всегда будете в\xA0курсе появился ли у\xA0вас новый клиент или покупатель, поэтому сможете оперативно среагировать.",
       imgUrl: TGBOT,
+      posterUrl: TGBOTPoster,
     },
     {
       serviceName: "Запуск рекламы",
       serviceText:
         "После завершения проекта, мы осуществляем гибкую и точную настройку рекламной компании в\xA0Яндекс Директ. Вы сможете получить первых посетителей в\xA0ближайшие сутки. Полученные данные и статистика позволят провести анализ для дальнейшего успешного развития своего проекта.",
       imgUrl: MARKETING,
+      posterUrl: MARKETINGPoster,
     },
   ];
   const serviceOnClick = (i) => {
@@ -89,7 +109,26 @@ export const Desk = () => {
       <div className={style.deskCard}>
         <div className={style.deskCardLeft}>
           <div className={style.deskCardLeftImg}>
-            {<img src={services[activeService].imgUrl} alt="" />}
+            {/* <ReactPlayer
+              url={services[activeService].imgUrl}
+              width={width < 700 ? "auto" : "460"}
+              height={width < 700 ? "auto" : "320"}
+              playing="true"
+              loop="true"
+              volume="0"
+              fileConfig={{ attributes: { poster: services[activeService].posterUrl } }}
+            /> */}
+            <video
+              key={services[activeService].imgUrl}
+              width={width < 700 ? "100%" : "460"}
+              height={width < 700 ? "100%" : "320"}
+              autoPlay
+              muted
+              loop
+              poster={services[activeService].posterUrl}
+            >
+              <source src={services[activeService].imgUrl} />
+            </video>
           </div>
         </div>
         <div className={style.deskCardRight}>
