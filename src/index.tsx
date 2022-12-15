93d96aa43f9443502a57435f7a6061d21c411611
Dmitry Drozdov <drozdov@algont.ru>
2022-12-15 16:54:55 +0300

    Revert "Fonts"
    This reverts commit 2f1e0c3f35931c10fa4ca02202b1e901de764dcf.

--- src/index.tsx
+++ src/index.tsx
@@ -2,24 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import "@fontsource/raleway/100.css"
-import "@fontsource/raleway/200.css"
-import "@fontsource/raleway/300.css"
-import "@fontsource/raleway/400.css"
-import "@fontsource/raleway/500.css"
-import "@fontsource/raleway/600.css"
-import "@fontsource/raleway/700.css"
-import "@fontsource/raleway/800.css"
-import "@fontsource/raleway/900.css"
-import "@fontsource/montserrat/100.css"
-import "@fontsource/montserrat/200.css"
-import "@fontsource/montserrat/300.css"
-import "@fontsource/montserrat/400.css"
-import "@fontsource/montserrat/500.css"
-import "@fontsource/montserrat/600.css"
-import "@fontsource/montserrat/700.css"
-import "@fontsource/montserrat/800.css"
-import "@fontsource/montserrat/900.css"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
