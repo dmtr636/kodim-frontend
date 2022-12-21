bed05c3ecd3aea93a0e1ece81d0809bbb442da3c
dmtr636 <dmtr636@gmail.com>
2022-12-22 00:52:25 +0300

    MVP Calculator drawer

--- src/components/calculator/CalculatorPriceMobile/CalculatorPriceMobile.tsx
+++ src/components/calculator/CalculatorPriceMobile/CalculatorPriceMobile.tsx
@@ -3,40 +3,79 @@ import {observer} from "mobx-react-lite";
 import styles from "./style.module.scss"
 import classNames from "classnames";
 import {calculatorStore} from "../../../stores/calculatorStore";
+import {Box, Skeleton, styled, SwipeableDrawer, Typography} from "@mui/material";
+import {grey} from "@mui/material/colors";
+import {Global} from "@emotion/react";
+
+const Puller = styled(Box)(({ theme }) => ({
+    width: 30,
+    height: 6,
+    backgroundColor: theme.palette.mode === 'light' ? grey[300] : grey[900],
+    borderRadius: 3,
+    position: 'absolute',
+    top: 8,
+    left: 'calc(50% - 15px)',
+}));
+
+const drawerHeaderHeight = 60
+const bottomPanelHeight = 62
 
 const CalculatorPriceMobile = observer(() => {
     const store = calculatorStore
     const [expanded, setExpanded] = useState(false)
 
+    const [open, setOpen] = React.useState(false);
+
     return (
-        <div
-            className={classNames(
-                styles.container,
-                {[styles.containerExpanded]: expanded}
-            )}
-        >
-            <div className={styles.title}>
-                Список выбранных услуг
-            </div>
-            <button className={styles.arrowButton} onClick={() => setExpanded(!expanded)}>
-                <div className={styles.arrow}/>
-            </button>
-            {expanded &&
-                <div className={styles.positions}>
-                    {store.pricePositions.map(position =>
-                        <div className={styles.position} key={position.name}>
-                            <div className={styles.positionName}>
-                                {position.name}
-                            </div>
-                            <div className={styles.positionPrice}>
-                                {position.price.toLocaleString()} ₽
-                            </div>
-                            <div className={styles.divider}/>
+        <div>
+            <SwipeableDrawer
+                anchor="bottom"
+                open={open}
+                onClose={() => setOpen(false)}
+                onOpen={() => setOpen(true)}
+                swipeAreaWidth={drawerHeaderHeight + bottomPanelHeight}
+                disableSwipeToOpen={false}
+                ModalProps={{
+                    keepMounted: true,
+                }}
+                PaperProps={{
+                    style: {
+                        height: `calc(100% - ${drawerHeaderHeight + bottomPanelHeight}px)`,
+                        overflow: 'visible',
+                    }
+                }}
+            >
+                <div className={classNames(
+                    styles.container,
+                    {[styles.containerOpen]: open}
+                )}>
+                    <div
+                        className={styles.header}
+                    >
+                        <div className={styles.puller}/>
+                        <div className={styles.title}>
+                            Список выбранных услуг
                         </div>
-                    )}
+                    </div>
+                    <div className={styles.positions}>
+                        {store.pricePositions.map(position =>
+                            <div className={styles.position} key={position.name}>
+                                <div className={styles.positionName}>
+                                    {position.name}
+                                </div>
+                                <div className={styles.positionPrice}>
+                                    {position.price.toLocaleString()} ₽
+                                </div>
+                                <div className={styles.divider}/>
+                            </div>
+                        )}
+                    </div>
                 </div>
-            }
-            <div className={styles.bottom}>
+            </SwipeableDrawer>
+            <div className={classNames(
+                styles.bottomPanel,
+                {[styles.bottomPanelOpen]: open}
+            )}>
                 <div className={styles.divider}/>
                 <div className={styles.total}>
                     Итого
