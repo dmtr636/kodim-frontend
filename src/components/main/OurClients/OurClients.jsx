7128694351dd85daf2d5abc89f360318f6528839
KoMRaD1240 <komrad.noob@yandex.ru>
2022-12-22 17:45:47 +0300

    123

--- src/components/main/OurClients/OurClients.jsx
+++ src/components/main/OurClients/OurClients.jsx
@@ -6,9 +6,9 @@ export const OurClients = () => {
   const {width} = useWindowDimensions()
   return (
     <div className={style.wrapper}>
-      <div className={style.left}>
+      <h2 className={style.left}>
         {width < 700 ?<OurClientsMob/>:"Наши клиенты это\xA0—\xA0малый бизнес,\nи все те, кому нужен реально\n быстрый сайт"}
-      </div>
+      </h2>
       <div className={style.right}>Скорость вашего <br /> будущего <br /> сайта</div>
     </div>
   );
@@ -16,8 +16,8 @@ export const OurClients = () => {
 
 export const OurClientsMob = () => {
   return (
-    <>Наши клиенты <span style={{ whiteSpace: 'nowrap' }}>это — малый бизнес</span>, <span style={{ whiteSpace: 'nowrap' }}>и все те</span>, кому нужен реально
-    быстрый сайт</>
+    <h2>Наши клиенты <span style={{ whiteSpace: 'nowrap' }}>это — малый бизнес</span>, <span style={{ whiteSpace: 'nowrap' }}>и все те</span>, кому нужен реально
+    быстрый сайт</h2>
   )
 }
 
