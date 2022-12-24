67c2f71400001493a1b4fcc804b5df647d0c0141
KoMRaD1240 <komrad.noob@yandex.ru>
2022-12-24 13:22:40 +0300

    change ticker

--- src/components/main/Ticker/Ticker.jsx
+++ src/components/main/Ticker/Ticker.jsx
@@ -2,28 +2,20 @@ import React from "react";
 import style from "./Ticker.module.scss";
 import Marquee from "react-fast-marquee";
 import { TickerLinks } from "./tickerLinks/TickerLinks";
+import {observer} from "mobx-react-lite";
+import { projectsStore } from "../../../stores/projectsStore";
 
-export const Ticker = () => {
-  const cases = [
-    { caseName: "Кафе", link: "/projects"},
-    { caseName: "Агенство недвижимости", link: "/projects" },
-    { caseName: "Водитель газели", link: "/projects" },
-    { caseName: "Строительная компания", link: "/projects" },
-    { caseName: "Дизайнер", link: "/projects" },
-    { caseName: "Продажа дома", link: "/projects" },
-    { caseName: "Кафе", link: "/projects" },
-    { caseName: "Агенство недвижимости", link: "/projects" },
-    { caseName: "Водитель газели", link: "/projects"},
-    { caseName: "Строительная компания", link: "/projects"},
-    { caseName: "Дизайнер", link: "/projects"},
-    { caseName: "Продажа дома", link: "/projects" },
-  ];
-  const caseElement= cases.map((el,i)=><TickerLinks key={i+el.caseName}  link={el.link} caseName={el.caseName}/>)
+export const Ticker = observer(() => {
+  const cases = projectsStore.cases.filter(item => item.in_marquee)
+  console.log(cases)
+  /* const casesNew=cases.filter(case => case.in_marquee) */
+  const caseElement= cases.map((el,i)=><TickerLinks key={i+el.marquee_name}  link={"/cases/"+el.id} caseName={el.marquee_name}/>)
   return (
     <div className={style.wrapper}>
       <Marquee gradient={false}  >
       {caseElement}
+      {caseElement}
       </Marquee>
     </div>
   );
-};
+});
