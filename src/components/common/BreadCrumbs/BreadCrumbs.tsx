d41054104b488da21a3f1a831f4edd363deb1d44
dmtr636 <dmtr636@gmail.com>
2022-12-25 19:56:54 +0300

    Animations

--- src/components/common/BreadCrumbs/BreadCrumbs.tsx
+++ src/components/common/BreadCrumbs/BreadCrumbs.tsx
@@ -1,42 +1,60 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import styles from "./BreadCrumbs.module.scss"
 import {NavLink, useLocation} from "react-router-dom";
 import useBreadcrumbs from "use-react-router-breadcrumbs";
 import {createRoutes} from "../../../routes/routes";
 import {projectsStore} from "../../../stores/projectsStore";
 import {observer} from "mobx-react-lite";
+import {CSSTransition} from 'react-transition-group'
 
 
 const BreadCrumbs = observer(() => {
     const breadcrumbs = useBreadcrumbs(createRoutes(projectsStore));
     const location = useLocation()
+    const nodeRef = useRef(null)
 
-    if (location.pathname === "/") {
-        return null
-    }
-
-    if (!projectsStore.projects.length || !projectsStore.cases.length) {
-        return null
-    }
+    let isShowWrapper = location.pathname !== "/"
+    let isShowBreadcrumbs = projectsStore.projects.length && projectsStore.cases.length
 
     return (
-        <div className={styles.wrapper}>
-            <div className={styles.content}>
-                {breadcrumbs.map((item, index) =>
-                    <React.Fragment key={item.key}>
-                        {index > 0 &&
-                            <div className={styles.separator}/>
-                        }
-                        <NavLink
-                            to={item.match.pathname}
-                            className={styles.link}
-                        >
-                            {item.breadcrumb}
-                        </NavLink>
-                    </React.Fragment>
-                )}
+        <CSSTransition
+            nodeRef={nodeRef}
+            in={isShowWrapper}
+            timeout={{
+                enter: 150,
+                exit: 50
+            }}
+            classNames={{
+                enter: styles.wrapperEnter,
+                enterActive: styles.wrapperEnterActive,
+                exit: styles.wrapperExit,
+                exitActive: styles.wrapperExitActive
+            }}
+            unmountOnExit
+        >
+            <div
+                className={styles.wrapper}
+                ref={nodeRef}
+            >
+                <div className={styles.content}>
+                    {isShowBreadcrumbs &&
+                        breadcrumbs.map((item, index) =>
+                            <React.Fragment key={item.key}>
+                                {index > 0 &&
+                                    <div className={styles.separator}/>
+                                }
+                                <NavLink
+                                    to={item.match.pathname}
+                                    className={styles.link}
+                                >
+                                    {item.breadcrumb}
+                                </NavLink>
+                            </React.Fragment>
+                        )
+                    }
+                </div>
             </div>
-        </div>
+        </CSSTransition>
     );
 });
 
