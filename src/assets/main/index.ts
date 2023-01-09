ce349ff1a3c10c26fbac3f557f724c76eac0310d
KoMRaD1240 <komrad.noob@yandex.ru>
2023-01-09 18:13:58 +0300

    fix banner

--- src/assets/main/index.ts
+++ src/assets/main/index.ts
@@ -1,3 +1,4 @@
-import houseBG from "./houseBG.svg"
-
-export {houseBG}
+/* import houseBG from "./houseBG.svg" */
+import bgBannerFull from "./bgBannerFull.webp"
+import bgBannerFullsvg from "./bgBannerFull.svg"
+export {bgBannerFull,bgBannerFullsvg}
--- src/components/main/Banner/Banner.tsx
+++ src/components/main/Banner/Banner.tsx
@@ -3,35 +3,33 @@ import style from "./Banner.module.scss";
 import Button from "../../common/Button/Button";
 import { useNavigate } from "react-router-dom";
 import useWindowDimensions from "../../../hooks/useWindowDimensions";
-
-
-
-
+import { bgBannerFull } from "../../../assets/main";
 
 export const Banner = () => {
   const navigate = useNavigate();
-  const {width} = useWindowDimensions()
-  const words = ["Скоростные", "Без загрузок", "Для всех"]
-  const [currentWord,setCurrentWord] =React.useState(words[0])
-  const [wordCount, setWordCount] =React.useState(1)
-  const changeWordFunc =()=>{
-    if(wordCount<2){
-      setCurrentWord(words[wordCount])
-      setWordCount(wordCount+1)
+  const { width } = useWindowDimensions();
+  const words = ["Скоростные", "Без загрузок", "Для всех"];
+  const [currentWord, setCurrentWord] = React.useState(words[0]);
+  const [wordCount, setWordCount] = React.useState(1);
+  const changeWordFunc = () => {
+    if (wordCount < 2) {
+      setCurrentWord(words[wordCount]);
+      setWordCount(wordCount + 1);
+    } else {
+      setWordCount(0);
+      setCurrentWord(words[wordCount]);
     }
-    else{
-      setWordCount(0)
-      setCurrentWord(words[wordCount])
-    }
-  }
-  setTimeout(changeWordFunc,1000)
+  };
+  setTimeout(changeWordFunc, 1000);
 
   return (
     <div className={style.wrapper}>
       <div className={style.bodybanner}>
         <div className={style.body}>
           <h1 className={style.h1}>
-            {width < 700 ?"Делаем\n не просто сайты\n а веб-приложения":"Делаем не просто сайты\n а веб-приложения"}
+            {width < 700
+              ? "Делаем\n не просто сайты\n а веб-приложения"
+              : "Делаем не просто сайты\n а веб-приложения"}
           </h1>
           <div className={style.text}>{currentWord}</div>
           <div className={style.button}>
@@ -39,6 +37,7 @@ export const Banner = () => {
               Рассчитать стоимость онлайн
             </Button>
           </div>
+          <div className={style.img}><img className={style.imgsize} src={bgBannerFull} alt="" /></div>
         </div>
       </div>
     </div>
