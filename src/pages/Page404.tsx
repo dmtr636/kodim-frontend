59da4c9c5fc3c02a01f1f1ec26ada9a455db0acd
dmtr636 <dmtr636@gmail.com>
2023-01-09 10:44:01 +0300

    Google Analytics, 404

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -1,22 +1,22 @@
 import React, {useEffect} from "react"
 import style from "./Page404/Page404.module.scss"
 import { Link } from "react-router-dom"
+import {Helmet} from "react-helmet";
 
 export const Page404 = () => {
-    useEffect(() => {
-        if (window.location.pathname !== "/404") {
-            window.location.pathname = "/404"
-        }
-    }, [])
-
     return (
-        <div className={style.wrapper}>
-            <h1 className={style.h1}>Упс, а это 404. Не{"\xA0"}задерживаемся</h1>
-            <Link to={"/"} className={style.link}>Главная</Link>
-            <Link to={"/cases"}  className={style.link}>Кейсы</Link>
-            <Link to={"/projects"}  className={style.link}>Наши проекты</Link>
-            <Link to={"/calculator"} className={style.link}>Калькулятор услуг</Link>
-            <Link to={"/#faq"}  className={style.link}>Вопросы</Link>
-        </div>
+        <>
+            <Helmet>
+                <meta name="robots" content="noindex" />
+            </Helmet>
+            <div className={style.wrapper}>
+                <h1 className={style.h1}>Упс, а это 404. Не{"\xA0"}задерживаемся</h1>
+                <Link to={"/"} className={style.link}>Главная</Link>
+                <Link to={"/cases"}  className={style.link}>Кейсы</Link>
+                <Link to={"/projects"}  className={style.link}>Наши проекты</Link>
+                <Link to={"/calculator"} className={style.link}>Калькулятор услуг</Link>
+                <Link to={"/#faq"}  className={style.link}>Вопросы</Link>
+            </div>
+        </>
     )
 }
