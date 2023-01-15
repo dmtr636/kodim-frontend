6044812d16aaf4ccbc9055ba852667b0143a3198
KoMRaD1240 <komrad.noob@yandex.ru>
2023-01-15 22:13:55 +0300

    fix text

--- src/components/main/OurClients/OurClients.jsx
+++ src/components/main/OurClients/OurClients.jsx
@@ -3,22 +3,17 @@ import useWindowDimensions from "../../../hooks/useWindowDimensions";
 import style from "./OurClients.module.scss";
 
 export const OurClients = () => {
-  const {width} = useWindowDimensions()
+  const { width } = useWindowDimensions();
   return (
     <div className={style.wrapper}>
       <h2 className={style.left}>
-        {width < 700 ?<OurClientsMob/>:"Наши клиенты это\xA0—\xA0малый бизнес,\nи все те, кому нужен реально\n быстрый сайт"}
+        Разработка сайтов для&nbsp;малого бизнеса и&nbsp;самозанятых
       </h2>
-      <div className={style.right}><div className={style.rightText}>Скорость вашего <br /> будущего <br /> сайта</div></div>
+      <div className={style.right}>
+        <div className={style.rightText}>
+          Скорость вашего <br /> будущего <br /> сайта
+        </div>
+      </div>
     </div>
   );
 };
-
-export const OurClientsMob = () => {
-  return (
-    <>Наши клиенты <span style={{ whiteSpace: 'nowrap' }}>это — малый бизнес</span>, <span style={{ whiteSpace: 'nowrap' }}>и все те</span>, кому нужен реально
-    быстрый сайт</>
-  )
-}
-
-
