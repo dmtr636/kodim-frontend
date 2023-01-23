50020a8702611884cbf4980b96956bda605726d3
Dmitry Drozdov <drozdov@algont.ru>
2023-01-23 16:08:50 +0300

    Meta information

--- src/components/common/Layout/Layout.tsx
+++ src/components/common/Layout/Layout.tsx
@@ -10,12 +10,8 @@ import Cookies from "../Cookies/Cookies";
 import {getMetaByPath} from "../../../constants/pageMeta";
 import { CSSTransition, SwitchTransition } from 'react-transition-group'
 import {navRoutes} from "../../../routes/navRoutes";
-import useBreadcrumbs from "use-react-router-breadcrumbs";
-import {createRoutes} from "../../../routes/routes";
-import {projectsStore} from "../../../stores/projectsStore";
 
 const Layout = () => {
-    const breadcrumbs = useBreadcrumbs(createRoutes(projectsStore))
     const location = useLocation()
     const outlet = useOutlet()
     const [width, setWidth] = useState(window.outerWidth)
@@ -38,32 +34,18 @@ const Layout = () => {
         window.scrollTo(0, 0)
     }, [location])
 
-    let title = meta?.title
-    const breadcrumb = breadcrumbs.slice(-1)[0].match.route?.breadcrumb
-    if (typeof breadcrumb === "string") {
-        title = title ?? breadcrumb
-    }
-
     return (
         <>
             <Helmet>
-                {title &&
-                    <title>{meta?.title ?? title}</title>
-                }
+                <title>{meta.title}</title>
+                <meta name={"description"} content={meta.description}/>
+                <meta name={"keywords"} content={meta.keywords}/>
+
                 {isTablet(width)
                     ? <meta name="viewport" content="width=1280"/>
                     : <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 }
             </Helmet>
-
-            {meta &&
-                <Helmet>
-                    <meta name={"description"} content={meta.description}/>
-                    <meta name={"keywords"} content={meta.keywords}/>
-                </Helmet>
-            }
-
-            <Cookies/>
 
             <div className={styles.layout}>
                 <Header/>
@@ -94,6 +76,8 @@ const Layout = () => {
 
                 <Footer/>
             </div>
+
+            <Cookies/>
         </>
     );
 };
