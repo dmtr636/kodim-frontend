3a551b2512cbd7b3284087a1d7a941858d8ee520
dmtr636 <dmtr636@gmail.com>
2023-02-02 21:16:48 +0300

    Test fonts

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import "@fontsource/montserrat/400.css"
-import "@fontsource/montserrat/500.css"
-import "@fontsource/montserrat/600.css"
-import "@fontsource/montserrat/700.css"
-import "@fontsource/montserrat/800.css"
-
-import "@fontsource/raleway/500.css"
-import "@fontsource/raleway/600.css"
-import "@fontsource/raleway/700.css"
-import "@fontsource/raleway/900.css"
+import "@fontsource/montserrat/variable.css"
+import "@fontsource/raleway/variable.css"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
