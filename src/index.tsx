a3957f50777e3eca61ba4bef76605f5af36da85a
dmtr636 <dmtr636@gmail.com>
2023-02-02 21:22:08 +0300

    Revert "Test fonts"
    This reverts commit 3a551b2512cbd7b3284087a1d7a941858d8ee520.

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import "@fontsource/montserrat/variable.css"
-import "@fontsource/raleway/variable.css"
+import "@fontsource/montserrat/400.css"
+import "@fontsource/montserrat/500.css"
+import "@fontsource/montserrat/600.css"
+import "@fontsource/montserrat/700.css"
+import "@fontsource/montserrat/800.css"
+
+import "@fontsource/raleway/500.css"
+import "@fontsource/raleway/600.css"
+import "@fontsource/raleway/700.css"
+import "@fontsource/raleway/900.css"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
